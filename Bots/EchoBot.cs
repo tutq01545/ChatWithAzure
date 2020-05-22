e3119ccc48fb81b2e4d64eec7d464f40bd573c75
Tran <tutran1@kpmg.com>
2020-05-22 16:00:38 +0200

    update EchoBot

--- Bots/EchoBot.cs
+++ Bots/EchoBot.cs
@@ -12,36 +12,31 @@ using Microsoft.Bot.Schema;
 namespace ChatWithMe.Bots
 {
     public class EchoBot : ActivityHandler
-    {
-        private static bool start = true;
-
+    {               
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
-            if (start)
+            var text = turnContext.Activity.Text;
+            if (text.Length == 0 || text == null)
             {
-                start = false;
                 string welcomeText = "Hello there, welcome to ChatWithMe. How can I help you?";
                 await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
             }
-
-            var text = turnContext.Activity.Text;            
-            TextProcess textProcess = new TextProcess();
-            string replyText = textProcess.Run(text);
-            await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+            else
+            {
+                TextProcess textProcess = new TextProcess();
+                string replyText = textProcess.Run(text);
+                await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+            }            
         }
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
         {
-            if (turnContext.Activity.ChannelId != "webchat" && turnContext.Activity.ChannelId != "directline")
+            string welcomeText = "Hello there, welcome to ChatWithMe. How can I help you?";
+            foreach (var member in membersAdded)
             {
-                string welcomeText = "Hello there, welcome to ChatWithMe. How can I help you?";
-                foreach (var member in membersAdded)
+                if (member.Id != turnContext.Activity.Recipient.Id)
                 {
-                    if (member.Id != turnContext.Activity.Recipient.Id && start)
-                    {
-                        start = false;
-                        await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
-                    }
+                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
                 }
             }
         }
