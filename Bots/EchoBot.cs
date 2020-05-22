8d73fc8f55e42f5cb914718907234b38697af6f3
Tran <tutran1@kpmg.com>
2020-05-22 16:14:02 +0200

    update EchoBot

--- Bots/EchoBot.cs
+++ Bots/EchoBot.cs
@@ -4,6 +4,7 @@
 // Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.6.2
 
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
@@ -12,27 +13,30 @@ using Microsoft.Bot.Schema;
 namespace ChatWithMe.Bots
 {
     public class EchoBot : ActivityHandler
-    {               
+    {
+        protected override async Task OnConversationUpdateActivityAsync(ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            await base.OnConversationUpdateActivityAsync(turnContext, cancellationToken);
+
+            if (turnContext.Activity.ChannelId == "directline")
+            {
+                string welcomeText = "Hello there, welcome to ChatWithMe. How can I help you? 1";
+                await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+            }
+        }
+
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
             var text = turnContext.Activity.Text;
             var type = turnContext.Activity.Type;
-            if (type == "welcomeEvent")
-            {
-                string welcomeText = "Hello there, welcome to ChatWithMe. How can I help you? 1" + text;
-                await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
-            }
-            else
-            {
-                TextProcess textProcess = new TextProcess();
-                string replyText = textProcess.Run(text);
-                await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
-            }            
+            TextProcess textProcess = new TextProcess();
+            string replyText = textProcess.Run(text);
+            await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);                       
         }
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
         {
-            string welcomeText = "Hello there, welcome to ChatWithMe. How can I help you?";
+            string welcomeText = "Hello there, welcome to ChatWithMe. How can I help you? 2";
             foreach (var member in membersAdded)
             {
                 if (member.Id != turnContext.Activity.Recipient.Id)
